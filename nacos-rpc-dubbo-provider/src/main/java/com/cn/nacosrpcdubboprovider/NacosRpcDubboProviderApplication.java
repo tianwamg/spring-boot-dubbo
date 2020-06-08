9c0cc14dd7ad7575351b4df927f3c8fbf9829e94
wangpp <987897856@qq.com>
2020-06-08 18:29:41 +0800

    nacos initial

---
+++ nacos-rpc-dubbo-provider/src/main/java/com/cn/nacosrpcdubboprovider/NacosRpcDubboProviderApplication.java
@@ -0,0 +1,13 @@
+package com.cn.nacosrpcdubboprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosRpcDubboProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosRpcDubboProviderApplication.class, args);
+    }
+
+}
---
+++ nacos-rpc-dubbo-provider/src/test/java/com/cn/nacosrpcdubboprovider/NacosRpcDubboProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.nacosrpcdubboprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosRpcDubboProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
